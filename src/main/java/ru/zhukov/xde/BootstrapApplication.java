eb264a442806cb1d13aa376348acb7664a876342
zulus23 <zulus23@gmail.com>
2017-05-26 15:31:30 +0300

    move fxml file to resources folder

--- src/main/java/ru/zhukov/xde/BootstrapApplication.java
+++ src/main/java/ru/zhukov/xde/BootstrapApplication.java
@@ -1,7 +1,11 @@
 package ru.zhukov.xde;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import ru.zhukov.xde.ui.XDEApplicationController;
 
 /**
  * Created by Gukov on 26.05.2017.
@@ -9,8 +13,24 @@ import javafx.stage.Stage;
 public class BootstrapApplication extends Application{
 
 
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(BootstrapApplication.class.getResource("/ru/zhukov/xde/ui/XDEApplicationView.fxml"));
+            XDEApplicationController controller = new XDEApplicationController();
+            fxmlLoader.setController(controller);
+            AnchorPane anchorPane = fxmlLoader.load();
+            Scene scene = new Scene(anchorPane);
+
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e ){
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
     }
 
---
+++ src/main/java/ru/zhukov/xde/ui/XDEApplicationController.java
@@ -0,0 +1,18 @@
+package ru.zhukov.xde.ui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Gukov on 26.05.2017.
+ */
+public class XDEApplicationController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

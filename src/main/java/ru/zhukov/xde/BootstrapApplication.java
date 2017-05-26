57c86c8a83ad624bf293c1f5e69baab4a9c8187f
zulus23 <zulus23@gmail.com>
2017-05-26 13:34:40 +0300

    add domain class Item and class start application

---
+++ src/main/java/ru/zhukov/xde/BootstrapApplication.java
@@ -0,0 +1,20 @@
+package ru.zhukov.xde;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Gukov on 26.05.2017.
+ */
+public class BootstrapApplication extends Application{
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/ru/zhukov/xde/domain/Item.java
@@ -0,0 +1,66 @@
+package ru.zhukov.xde.domain;
+
+/**
+ * Created by Gukov on 26.05.2017.
+ */
+public class Item {
+    private String item;
+    private String name;
+    private String nameFull;
+    private String edizm;
+    private String nds;
+    private String parent;
+
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameFull() {
+        return nameFull;
+    }
+
+    public void setNameFull(String nameFull) {
+        this.nameFull = nameFull;
+    }
+
+    public String getEdizm() {
+        return edizm;
+    }
+
+    public void setEdizm(String edizm) {
+        this.edizm = edizm;
+    }
+
+    public String getNds() {
+        return nds;
+    }
+
+    public void setNds(String nds) {
+        this.nds = nds;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+
+
+
+}
